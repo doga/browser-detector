8bd810ad155f0a40ff329d059675f4a9216596d6
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-03-17 00:32:36 +0100

    Conform to official UA string format, add browser subclasses

--- mod.mjs
+++ mod.mjs
@@ -1,40 +1,112 @@
 class Browser {
+  /** @type {{userAgentString:string, systemInfo:string[], platform:{name:string, version:string, details:(string|undefined)}, extensions:[{name:string, version:string}]}} */
   #info;
-  constructor(userAgentString, browserName, browserVersion, operatingSystem){
-    this.#info = {
-      userAgentString,
-      browserName,
-      browserVersion,
-      operatingSystem,
-      // isMobile
-    };
+
+  /**
+   * @param {{userAgentString:string, systemInfo:string[], platform:{name:string, version:string, details:(string|undefined)}, extensions:[{name:string, version:string}]}} arg 
+   **/
+  constructor(arg){
+    this.#info = arg;
   }
-  get userAgentString() {return this.#info.userAgentString;}
-  get browserName() {return this.#info.browserName;}
-  get browserVersion() {return this.#info.browserVersion;}
-  get operatingSystem() {return this.#info.operatingSystem;}
-  // get isMobile() {return this.#info.isMobile;}
+
+  /** @returns {string} */
+  get deviceType() {return this.#info.systemInfo[0];}
+
+  /** @returns {string[]} */
+  get systemInfo() {return this.#info.systemInfo;} 
+
+  /** @returns {string} */
+  get platformName() {return this.#info.platform.name;}
+
+  /** @returns {string} */
+  get platformVersion() {return this.#info.platform.version;}
+
+  /** @returns {string} */
+  get platformDetails() {return this.#info.platform.details;}
+
+  /** @returns {string} */
+  toString(){return this.#info.userAgentString;}
 }
 
-class ChromeDesktop extends Browser{
-  constructor(userAgentString, browserName, browserVersion, operatingSystem){
-    super(userAgentString, browserName, browserVersion, operatingSystem);
+
+// Desktop browsers that have extension support /////////////////////////////////////////
+class ChromiumDesktop extends Browser{
+  constructor(arg){
+    super(arg);
   }
 }
 
+class EdgeDesktop extends Browser{
+  constructor(arg){
+    super(arg);
+  }
+}
+
+class OperaDesktop extends Browser{
+  constructor(arg){
+    super(arg);
+  }
+}
+
+class FirefoxDesktop extends Browser{
+  constructor(arg){
+    super(arg);
+  }
+}
+
+class SafariDesktop extends Browser{
+  constructor(arg){
+    super(arg);
+  }
+}
+
+
+// Mobile browsers that have extension support /////////////////////////////////////////
 class FirefoxAndroid extends Browser{
-  constructor(userAgentString, browserName, browserVersion, operatingSystem){
-    super(userAgentString, browserName, browserVersion, operatingSystem);
+  constructor(arg){
+    super(arg);
   }
 }
 
 class SafariMobile extends Browser{
-  constructor(userAgentString, browserName, browserVersion, operatingSystem){
-    super(userAgentString, browserName, browserVersion, operatingSystem);
+  constructor(arg){
+    super(arg);
   }
 }
 
-const userAgentRegex = /^\s*(?<product>\S+)\s+\((?<platform>[^;)]+(;[^;)]+)*)\).*\s+(?<browserName>[^/]+)\/(?<browserVersion>\S+)$/;
+
+// Detector /////////////////////////////////////////
+
+/*
+Some user agents on macOS:
+- ⚠️ Brave: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36
+- ⚠️ Chrome: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36
+- ⚠️ Vivaldi: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36
+- Firefox: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0
+- Opera: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.65
+- Safari: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15
+- Microsoft Edge: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0
+*/
+
+/*
+Some user agents on iPhone:
+- Brave: Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1
+- Chrome: Mozilla/5.0 (iPhone; CPU iPhone OS 15_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/122.0.6261.89 Mobile/15E148 Safari/604.1
+- Firefox: Mozilla/5.0 (iPhone; CPU iPhone OS 15_8_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/123.4  Mobile/15E148 Safari/605.1.15
+- Opera: Mozilla/5.0 (iPhone; CPU iPhone OS 15_8_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.8 Mobile/15E148 Safari/604.1 OPT/4.6.0
+- Safari: Mozilla/5.0 (iPhone; CPU iPhone OS 15_8_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.6 Mobile/15E148 Safari/604.1
+- Microsoft Edge: Mozilla/5.0 (iPhone; CPU iPhone OS 15_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) EdgiOS/122.0.2365.86 Version/15.0 Mobile/15E148 Safari/604.1
+*/
+
+const regex = {
+  // User agent string. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+  // Debugged with https://regexr.com/
+  userAgent  : /^\s*(?<mozilla5>\S+)\s+\((?<systemInfo>[^)]+)\)\s+(?<platformName>[^/]+)\/(?<platformVersion>\S+)(\s+\((?<platformDetails>[^)]+)\))?(?<extensions>.*)$/, 
+
+  desktop    : /Windows|Mac|X11|Wayland/i, // X11|Wayland means Linux
+  android    : /Android/i,
+  appleMobile: /iPhone|iPad/i,
+};
 
 /**
  * Infers a web browser's type from its User-Agent header.
@@ -44,31 +116,60 @@ const userAgentRegex = /^\s*(?<product>\S+)\s+\((?<platform>[^;)]+(;[^;)]+)*)\).
 function detectBrowser(userAgentString){
   if(!(typeof userAgentString === 'string'))return null;
 
-  const match = userAgentString.match(userAgentRegex);
-  if(!match)return null;
+  try {
+    const match = userAgentString.match(regex.userAgent);
+    if(!match)return null;
 
-  const
-  platform        = match.groups.platform.split(';').map(item => item.trim()),
-  operatingSystem = platform[0],
-  // isMobile        = !!platform.find(item => item.match(/mobile/i)),
-  browserName     = match.groups.browserName,
-  browserVersion  = match.groups.browserVersion;
+    const
+    systemInfo = match.groups.systemInfo.trim().replaceAll(/\s+/g, ' ').split(';').map(item => item.trim()),
+    deviceType = systemInfo[0],
+    platform   = 
+      {
+        name   : match.groups.platformName,
+        version: match.groups.platformVersion,
+        details: match.groups.platformDetails
+      },
+    extensions = 
+      match.groups.extensions.trim().replaceAll(/\s+/g, ' ').split(' ')
+      .map(extension => extension.split('/'))
+      .map(extension => ({name: extension[0], version: extension[1]})),
+    hasExtension = name => extensions.find(ext => ext.name === name),
+    arg = 
+      {
+        userAgentString,
+        systemInfo,
+        platform,
+        extensions
+      };
 
-  if (browserName === 'Chrome' && operatingSystem.match(/Windows|Mac|X11|Wayland/i)) { // X11|Wayland means Linux
-    return new ChromeDesktop(userAgentString,browserName,browserVersion,operatingSystem);
-  } else if (browserName === 'Firefox' && operatingSystem.match(/Android/i)) {
-    return new FirefoxAndroid(userAgentString,browserName,browserVersion,operatingSystem);
-  } else if (browserName === 'Safari' && operatingSystem.match(/iPhone|iPad/i)) {
-    return new SafariMobile(userAgentString,browserName,browserVersion,operatingSystem);
-  } else {
-    return new Browser(userAgentString,browserName,browserVersion,operatingSystem);
+    // console.debug(`systemInfo: ${JSON.stringify(systemInfo)}`);
+    // console.debug(`platform: ${JSON.stringify(platform)}`);
+    // console.debug(`extensions: ${JSON.stringify(extensions)}`);
+
+    if (deviceType.match(regex.desktop)) {
+      if (hasExtension('Firefox')) return new FirefoxDesktop(arg);
+      if (hasExtension('OPR')) return new OperaDesktop(arg);
+      if (hasExtension('Edg')) return new EdgeDesktop(arg);
+      if (hasExtension('Chrome')) return new ChromiumDesktop(arg);
+      if (hasExtension('Safari')) return new SafariDesktop(arg);
+    } else if(deviceType.match(regex.android)) {
+      if (hasExtension('Firefox')) return new FirefoxAndroid(arg);
+    } else if(deviceType.match(regex.appleMobile)) {
+      if (
+        hasExtension('Safari') && !['CriOS', 'FxiOS', 'EdgiOS', 'OPT'].find(ext => hasExtension(ext))
+      ) return new SafariMobile(arg); // Warning: actual browser may be another browser such as Brave (which doesn't support extensions!!!)
+    }
+    return new Browser(arg);
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-
 }
-
 
 
 export {
   detectBrowser,
-  Browser, ChromeDesktop, FirefoxAndroid, SafariMobile
+  Browser, 
+  ChromiumDesktop, EdgeDesktop, OperaDesktop, FirefoxDesktop, SafariDesktop,
+  FirefoxAndroid, SafariMobile
 };
